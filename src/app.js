d05aa2ef79c2be41bb745ccc14a1c911d6bc7517
Jessica Ganda <jganda007@gmail.com>
2025-11-30 16:11:03 +0700

    Add index.js, app.config.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
+import home from "./routes/index.js";
 import apiRoutes from "./routes/apiRoutes.js";
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/", home);
 app.use("/api", apiRoutes);
 
 export default app;
---
+++ src/config/app.config.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+    port: 3000,
+    env: "development",
+    appName: "Transformers Facts"
+}
